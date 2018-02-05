4acc4937b0fc33205d868ad6f902c254e645edc2
Kris Sakarias <ksakarias@gmail.com>
2018-02-05 13:51:38 -0800

    started filtering by property

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ function Restaurant(name, cuisine, neighborhood, parking, price) {
 }
 
 new Restaurant('Garam Masala', 'Indian', 'University District', false, 1);
+new Restaurant('Chili\'s South Indian', 'Indian', 'University District', false, 1);
+new Restaurant('Annapurna', 'Indian', 'Capitol Hill', false, 1);
+new Restaurant('Roti', 'Indian', 'Queen Anne', false, 1);
 new Restaurant('Shawarma King', 'Mediterranean', 'University District', false, 1);
 new Restaurant('Un Bien', 'Cuban', 'Ballard', false, 1);
 new Restaurant('Pam\'s Kitchen', 'Carribean', 'University District', false, 2);
@@ -21,10 +24,15 @@ new Restaurant('Blue Water Taco Grill', 'Mexican', 'Queen Anne', false, 1);
 
 
 function createList() {
+  var cuisine = JSON.parse(localStorage.getItem('cuisine'));
+  var neighborhood = JSON.parse(localStorage.getItem('neighborhood'));
+
   for (var i = 0; i < Restaurant.allRestaurants.length; i++) {
-    var sectionEl = document.createElement('section');
-    sectionEl.innerHTML = '<h1>' + Restaurant.allRestaurants[i].name + '</h1>';
-    mainEl.appendChild(sectionEl);
+    if (cuisine === Restaurant.allRestaurants[i].cuisine && neighborhood === Restaurant.allRestaurants[i].neighborhood) {
+      var sectionEl = document.createElement('section');
+      sectionEl.innerHTML = '<h1>' + Restaurant.allRestaurants[i].name + '</h1>';
+      mainEl.appendChild(sectionEl);
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 
 var formEl = document.getElementById('choose-food');
 
-formEl.addEventListener('submit', function(e) {
+formEl.addEventListener('submit', setValues);
+
+function setValues(e) {
   e.preventDefault();
-  window.location.href = 'response.html';
-});
+  localStorage.setItem('cuisine', JSON.stringify(e.target[1].value));
+  localStorage.setItem('neighborhood', JSON.stringify(e.target[3].value));
+  window.location = 'response.html';
+}
